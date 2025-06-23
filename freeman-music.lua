66233057ef811a9fce903f4e1258a464cf6bc711
Freeman4137 <trulyn400@gmail.com>
2025-06-23 11:04:21 -0300

    Atualizar o freeman-music.lua

--- freeman-music.lua
+++ freeman-music.lua
@@ -3,6 +3,8 @@ local musicIDs = {
     ["2"] = 99409598156364,
     ["3"] = 133900561957103,
     ["4"] = 93768636184697,
+    ["5"] = 72334211564889,
+    ["6"] = 140296674808875,
 }
 
 local player = game:GetService("Players").LocalPlayer
@@ -190,7 +192,7 @@ musicListFrame.Visible = false
 local musicListLabel = Instance.new("TextLabel", musicListFrame)
 musicListLabel.Size = UDim2.new(1, -20, 1, -20)
 musicListLabel.Position = UDim2.new(0, 10, 0, 10)
-musicListLabel.Text = "[1] - Funk da Praia, added by Freeman\n[2] - Retrolam Funk, added by Freeman\n[3] - Trash Funk, added by Freeman\n[4] - 2609 (Jersey Club), added by Freeman"
+musicListLabel.Text = "[1] - Funk da Praia, added by Freeman\n[2] - Retrolam Funk, added by Freeman\n[3] - Trash Funk, added by Freeman\n[4] - 2609 (Jersey Club), added by Freeman,\n[5] - Met Her on The Internet, added by Freeman\n[6] - Old Swing Funk, added by Freeman"
 musicListLabel.Font = Enum.Font.Gotham
 musicListLabel.TextColor3 = Color3.fromRGB(255,255,255)
 musicListLabel.TextSize = 10
@@ -225,7 +227,7 @@ muteGameSoundsButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 Instance.new("UICorner", muteGameSoundsButton).CornerRadius = UDim.new(0, 10)
 
 local buttons = {}
-for _, name in ipairs({"1", "2", "3", "4"}) do
+for _, name in ipairs({"1", "2", "3", "4", "5", "6"}) do
     local id = musicIDs[name]
     local btn = Instance.new("TextButton")
     btn.Size = UDim2.new(0, 100, 0, 40)
